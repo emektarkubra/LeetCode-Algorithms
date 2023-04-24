2e25eda28fea3e041c509aa1d0efc9be5d7c4c88
Kübra <emektarrkubra@gmail.com>
2023-04-24 15:03:07 +0300

    added solution

---
+++ remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums                         
+ * @return {number}                              
+ */
+var removeDuplicates = function (nums) {
+
+    const expectedNums = [].concat(nums);
+    nums.splice(0, nums.length);
+    for (let i = 0; i < expectedNums.length; i++) {
+        if (!nums.includes(expectedNums[i])) {
+            nums.push(expectedNums[i]);
+        }
+    }
+    return nums.length;
+};
